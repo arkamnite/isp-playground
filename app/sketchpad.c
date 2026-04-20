3866961cceebc3e5553f5a5c88a19d6c7d898f55
Akram Ahmad <bulgeypip786@gmail.com>
2026-04-20 01:39:24 +0100

    rename some structs, add notes

--- app/sketchpad.c
+++ app/sketchpad.c
@@ -4,32 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct img_buffer {
+enum pixel_t { PIXEL_U8 };
+
+struct image {
   unsigned int height;
   unsigned int width;
   void *data;
+  enum pixel_t data_t;
 };
 
-struct pixel {
+struct pixel_u8 {
   unsigned char r;
   unsigned char g;
   unsigned char b;
 };
 
-void print_pixel(struct pixel *in) {
+void print_pixel(struct pixel_u8 *in) {
   if (in == NULL) {
     return;
   }
   printf("Pixel %p: (%d, %d, %d)\n", &in, in->r, in->g, in->b);
 }
 
-bool pixel_eq(struct pixel *a, struct pixel *b) {
+bool pixel_eq(struct pixel_u8 *a, struct pixel_u8 *b) {
   return ((a->r == b->r) && (a->g == b->g) && (a->b == b->b));
 }
 
-bool init_img_buffer(struct img_buffer *img, unsigned int height,
+bool init_img_buffer(struct image *img, unsigned int height,
                      unsigned int width) {
-  img->data = malloc(height * width * sizeof(struct pixel));
+  img->data = malloc(height * width * sizeof(struct pixel_u8));
 
   if (img->data) {
     img->height = height;
@@ -39,7 +42,7 @@ bool init_img_buffer(struct img_buffer *img, unsigned int height,
   return (img->data);
 }
 
-void free_img_buffer(struct img_buffer *img) {
+void free_img_buffer(struct image *img) {
   // printf("data pointer: %p\n", img->data);
   img->height = 0;
   img->width = 0;
@@ -47,13 +50,13 @@ void free_img_buffer(struct img_buffer *img) {
   img->data = NULL;
 }
 
-void print_img_buffer(struct img_buffer *img) {
+void print_img_buffer(struct image *img) {
   printf("Image height: %u\n", img->height);
   printf("Image width: %u\n", img->width);
   printf("Data Pointer: %p\n", img->data);
   if (img->data) {
     printf("Buffer size in pixels: %zd\n",
-           (img->height * img->width) / sizeof(struct pixel));
+           (img->height * img->width) / sizeof(struct pixel_u8));
   }
 }
 
@@ -69,43 +72,43 @@ void print_img_buffer(struct img_buffer *img) {
  * Into this:
  * 0 1 2 3 4 5 6 7 8
  */
-unsigned int calculate_index(struct img_buffer *img, unsigned int x,
+unsigned int calculate_index(struct image *img, unsigned int x,
                              unsigned int y) {
   return y * img->width + x;
 }
 
-bool write_to_buf(struct img_buffer *img_out, struct pixel *in, unsigned int x,
-                  unsigned int y) {
+bool write_img_pixel_u8(struct image *img_out, struct pixel_u8 *in,
+                        unsigned int x, unsigned int y) {
   if (y >= img_out->height || x >= img_out->width) {
     return false;
   }
 
   unsigned int index = calculate_index(img_out, x, y);
-  struct pixel *pixels = (struct pixel *)img_out->data;
-  memcpy(pixels + index, in, sizeof(struct pixel));
+  struct pixel_u8 *pixels = (struct pixel_u8 *)img_out->data;
+  memcpy(pixels + index, in, sizeof(struct pixel_u8));
   return true;
 }
 
-bool read_buf(struct img_buffer *img_in, struct pixel *out, unsigned int x,
-              unsigned int y) {
+bool read_img_pixel_u8(struct image *img_in, struct pixel_u8 *out,
+                       unsigned int x, unsigned int y) {
   if (y >= img_in->height || x >= img_in->width) {
     return false;
   }
 
   unsigned int index = calculate_index(img_in, x, y);
-  struct pixel *pixels = (struct pixel *)img_in->data;
-  memcpy(out, pixels + index, sizeof(struct pixel));
+  struct pixel_u8 *pixels = (struct pixel_u8 *)img_in->data;
+  memcpy(out, pixels + index, sizeof(struct pixel_u8));
   return true;
 }
 
-void test_pixel() {
-  struct img_buffer img = {0};
-  struct pixel my_pixel = (struct pixel){
+void test_pixel_u8() {
+  struct image img = {0};
+  struct pixel_u8 my_pixel = (struct pixel_u8){
       .r = 123,
       .g = 100,
       .b = 50,
   };
-  struct pixel read_pixel = {0};
+  struct pixel_u8 read_pixel = {0};
 
   print_pixel(&my_pixel);
   print_pixel(&read_pixel);
@@ -113,10 +116,10 @@ void test_pixel() {
   if (!init_img_buffer(&img, 100, 100))
     goto cleanup;
 
-  if (!write_to_buf(&img, &my_pixel, 0, 0))
+  if (!write_img_pixel_u8(&img, &my_pixel, 0, 0))
     goto cleanup;
 
-  if (!read_buf(&img, &read_pixel, 0, 0))
+  if (!read_img_pixel_u8(&img, &read_pixel, 0, 0))
     goto cleanup;
 
   print_pixel(&my_pixel);
@@ -132,7 +135,14 @@ cleanup:
   free_img_buffer(&img);
 }
 
-bool write_ppm(const char *path, struct img_buffer *img) {
+/**
+ * TODO: This currently expects the image data to contain only PPM pixels.
+ * Really, we should plan to convert between different pixel formats as the type
+ * of data in the image buffer changes too (e.g. Bayer pattern -> pixels). This
+ * is needed because PPM pixels are inherently capped at 255 and thus we will be
+ * needlessly discarding data when reading a RAW file.
+ */
+bool write_ppm(const char *path, struct image *img) {
   char int_buffer[60] = {0};
   FILE *fp = fopen(path, "w");
   if (!fp || !img || !img->data) {
@@ -148,8 +158,8 @@ bool write_ppm(const char *path, struct img_buffer *img) {
   for (int h = 0; h < img->height; h++) {
     for (int w = 0; w < img->width; w++) {
       char pixel_buf[100] = {0};
-      struct pixel pxl = (struct pixel){0};
-      read_buf(img, &pxl, w, h);
+      struct pixel_u8 pxl = (struct pixel_u8){0};
+      read_img_pixel_u8(img, &pxl, w, h);
       sprintf(pixel_buf, "%u %u %u", pxl.r, pxl.g, pxl.b);
       fputs(pixel_buf, fp);
     }
@@ -159,20 +169,23 @@ bool write_ppm(const char *path, struct img_buffer *img) {
   return true;
 }
 
+// TODO: extract Bayer RAW from DNG image.
+bool dng_to_bayer(const char *file, struct image *img) { return false; }
+
 int main() {
   // create buffer on the stack
-  struct img_buffer my_buf = {0};
+  struct image my_buf = {0};
   unsigned int limit = 100;
   init_img_buffer(&my_buf, limit, limit);
 
   for (int h = 0; h < my_buf.height; h++) {
     for (int w = 0; w < my_buf.width; w++) {
-      struct pixel pxl = (struct pixel){
+      struct pixel_u8 pxl = (struct pixel_u8){
           .r = (char)h + w,
           .g = (char)h + w,
           .b = (char)h + w,
       };
-      write_to_buf(&my_buf, &pxl, w, h);
+      write_img_pixel_u8(&my_buf, &pxl, w, h);
     }
   }
 
