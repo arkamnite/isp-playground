d67f9062941c09b05765a991d8a03afa4708d617
Akram Ahmad <bulgeypip786@gmail.com>
2026-04-20 19:45:21 +0100

    ppm: use 16-bit instead of 8-bit pixels

--- app/sketchpad.c
+++ app/sketchpad.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum pixel_t { PIXEL_U8 };
+enum pixel_t { PIXEL_U16 };
 
 struct image {
   unsigned int height;
@@ -13,26 +13,26 @@ struct image {
   enum pixel_t data_t;
 };
 
-struct pixel_u8 {
-  unsigned char r;
-  unsigned char g;
-  unsigned char b;
+struct pixel_u16 {
+  uint16_t r;
+  uint16_t g;
+  uint16_t b;
 };
 
-void print_pixel(struct pixel_u8 *in) {
+void print_pixel(struct pixel_u16 *in) {
   if (in == NULL) {
     return;
   }
-  printf("Pixel %p: (%d, %d, %d)\n", &in, in->r, in->g, in->b);
+  printf("Pixel %p: (%u, %u, %u)\n", &in, in->r, in->g, in->b);
 }
 
-bool pixel_eq(struct pixel_u8 *a, struct pixel_u8 *b) {
+bool pixel_eq(struct pixel_u16 *a, struct pixel_u16 *b) {
   return ((a->r == b->r) && (a->g == b->g) && (a->b == b->b));
 }
 
 bool init_img_buffer(struct image *img, unsigned int height,
                      unsigned int width) {
-  img->data = malloc(height * width * sizeof(struct pixel_u8));
+  img->data = malloc(height * width * sizeof(struct pixel_u16));
 
   if (img->data) {
     img->height = height;
@@ -56,7 +56,7 @@ void print_img_buffer(struct image *img) {
   printf("Data Pointer: %p\n", img->data);
   if (img->data) {
     printf("Buffer size in pixels: %zd\n",
-           (img->height * img->width) / sizeof(struct pixel_u8));
+           (img->height * img->width) / sizeof(struct pixel_u16));
   }
 }
 
@@ -77,38 +77,38 @@ unsigned int calculate_index(struct image *img, unsigned int x,
   return y * img->width + x;
 }
 
-bool write_img_pixel_u8(struct image *img_out, struct pixel_u8 *in,
-                        unsigned int x, unsigned int y) {
+bool write_img_pixel_u16(struct image *img_out, struct pixel_u16 *in,
+                         unsigned int x, unsigned int y) {
   if (y >= img_out->height || x >= img_out->width) {
     return false;
   }
 
   unsigned int index = calculate_index(img_out, x, y);
-  struct pixel_u8 *pixels = (struct pixel_u8 *)img_out->data;
-  memcpy(pixels + index, in, sizeof(struct pixel_u8));
+  struct pixel_u16 *pixels = (struct pixel_u16 *)img_out->data;
+  memcpy(pixels + index, in, sizeof(struct pixel_u16));
   return true;
 }
 
-bool read_img_pixel_u8(struct image *img_in, struct pixel_u8 *out,
-                       unsigned int x, unsigned int y) {
+bool read_img_pixel_u16(struct image *img_in, struct pixel_u16 *out,
+                        unsigned int x, unsigned int y) {
   if (y >= img_in->height || x >= img_in->width) {
     return false;
   }
 
   unsigned int index = calculate_index(img_in, x, y);
-  struct pixel_u8 *pixels = (struct pixel_u8 *)img_in->data;
-  memcpy(out, pixels + index, sizeof(struct pixel_u8));
+  struct pixel_u16 *pixels = (struct pixel_u16 *)img_in->data;
+  memcpy(out, pixels + index, sizeof(struct pixel_u16));
   return true;
 }
 
-void test_pixel_u8() {
+void test_pixel_u16() {
   struct image img = {0};
-  struct pixel_u8 my_pixel = (struct pixel_u8){
+  struct pixel_u16 my_pixel = (struct pixel_u16){
       .r = 123,
       .g = 100,
       .b = 50,
   };
-  struct pixel_u8 read_pixel = {0};
+  struct pixel_u16 read_pixel = {0};
 
   print_pixel(&my_pixel);
   print_pixel(&read_pixel);
@@ -116,10 +116,10 @@ void test_pixel_u8() {
   if (!init_img_buffer(&img, 100, 100))
     goto cleanup;
 
-  if (!write_img_pixel_u8(&img, &my_pixel, 0, 0))
+  if (!write_img_pixel_u16(&img, &my_pixel, 0, 0))
     goto cleanup;
 
-  if (!read_img_pixel_u8(&img, &read_pixel, 0, 0))
+  if (!read_img_pixel_u16(&img, &read_pixel, 0, 0))
     goto cleanup;
 
   print_pixel(&my_pixel);
@@ -136,11 +136,8 @@ cleanup:
 }
 
 /**
- * TODO: This currently expects the image data to contain only PPM pixels.
- * Really, we should plan to convert between different pixel formats as the type
- * of data in the image buffer changes too (e.g. Bayer pattern -> pixels). This
- * is needed because PPM pixels are inherently capped at 255 and thus we will be
- * needlessly discarding data when reading a RAW file.
+ * TODO: We will have to make sure that the data format within the image buffer
+ * is correct.
  */
 bool write_ppm(const char *path, struct image *img) {
   char int_buffer[60] = {0};
@@ -149,17 +146,18 @@ bool write_ppm(const char *path, struct image *img) {
     return false;
   }
 
-  /* PPM format has a 'maxval' field of 255. */
+  /* The old PPM format has a 'maxval' field of 255, but newer formats allow
+   * 16-bit values now. */
   fputs("P6\n", fp);
   sprintf(int_buffer, "%u %u\n", img->width, img->height);
   fputs(int_buffer, fp);
-  fputs("255\n", fp);
+  fputs("65535\n", fp);
 
   for (int h = 0; h < img->height; h++) {
     for (int w = 0; w < img->width; w++) {
       char pixel_buf[100] = {0};
-      struct pixel_u8 pxl = (struct pixel_u8){0};
-      read_img_pixel_u8(img, &pxl, w, h);
+      struct pixel_u16 pxl = (struct pixel_u16){0};
+      read_img_pixel_u16(img, &pxl, w, h);
       sprintf(pixel_buf, "%u %u %u", pxl.r, pxl.g, pxl.b);
       fputs(pixel_buf, fp);
     }
@@ -170,26 +168,28 @@ bool write_ppm(const char *path, struct image *img) {
 }
 
 // TODO: extract Bayer RAW from DNG image.
+// Need to iterate over headers, find the right SubIFDs, and then follow the
+// breadcrumbs to the RAW data.
 bool dng_to_bayer(const char *file, struct image *img) { return false; }
 
 int main() {
-  // create buffer on the stack
   struct image my_buf = {0};
-  unsigned int limit = 100;
-  init_img_buffer(&my_buf, limit, limit);
+  unsigned int limit = 6552;
+  init_img_buffer(&my_buf, limit / 2, limit);
 
   for (int h = 0; h < my_buf.height; h++) {
     for (int w = 0; w < my_buf.width; w++) {
-      struct pixel_u8 pxl = (struct pixel_u8){
-          .r = (char)h + w,
-          .g = (char)h + w,
-          .b = (char)h + w,
+      struct pixel_u16 pxl = (struct pixel_u16){
+          .r = (uint16_t)h + w,
+          .g = (uint16_t)h + w,
+          .b = (uint16_t)h + w,
       };
-      write_img_pixel_u8(&my_buf, &pxl, w, h);
+      write_img_pixel_u16(&my_buf, &pxl, w, h);
     }
   }
 
-  write_ppm("../out.ppm", &my_buf);
+  write_ppm("../res/out.ppm", &my_buf);
+
   // test_pixel();
   free_img_buffer(&my_buf);
   return 0;
